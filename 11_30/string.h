3132a57c7ed16aadee43539e0f3b5bf824cbdaca
Nancatcat <2710514756@qq.com>
2023-12-16 17:32:14 +0800

    测试2

---
+++ 11_30/string.h
@@ -0,0 +1,258 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#pragma once
+#include<assert.h>
+
+
+
+namespace bit
+{
+	class string
+	{
+	public:
+		typedef char* iterator;
+		typedef const char* const_iterator;
+		iterator begin()
+		{
+			return _str;
+		}
+		iterator end()
+		{
+			return _str + _size;
+		}
+		const_iterator begin() const
+		{
+			return _str;
+		}
+		const_iterator end() const
+		{
+			return _str + _size;
+		}
+		string(const char* str = "")
+		{
+			_size = strlen(str);
+			_capacity = _size;
+			_str = new char[_capacity + 1];
+			strcpy(_str, str);
+		}
+		string(const string& s)
+		{
+			_str = new char[s._capacity + 1];
+			strcpy(_str, s._str);
+			_size = s._size;
+			_capacity = s._capacity;
+		}
+		string& operator=(const string& s)
+		{
+			if (this != &s)
+			{
+				char* tmp = new char[s._capacity + 1];
+				strcpy(tmp, s._str);
+				delete[] _str;
+
+				_str = tmp;
+				_size = s._size;
+				_capacity = s._capacity;
+			}
+			return *this;
+		}
+		~string()
+		{
+			delete[] _str;
+			_str = nullptr;
+			_size = 0;
+			_capacity = 0;
+		}
+
+		const char* c_str() const
+		{
+			return _str;
+		}
+
+		size_t size() const
+		{
+			return _size;
+		}
+
+		const char& operator[](size_t pos) const
+		{
+			assert(pos <= _size);
+
+			return _str[pos];
+		}
+
+		char& operator[](size_t pos)
+		{
+			assert(pos <= _size);
+
+			return _str[pos];
+		}
+
+		void reserve(size_t n)
+		{
+			if (n > _capacity)
+			{
+				char* tmp = new char[n + 1];
+				strcpy(tmp, _str);
+				delete[] _str;
+				_str = tmp;
+				_capacity = n;
+			}
+		}
+
+		void push_back(char ch)
+		{
+			if (_size == _capacity)
+			{
+				size_t newCapacity = _capacity == 0 ? 4 : _capacity * 2;
+				reserve(newCapacity);
+			}
+
+			_str[_size] = ch;
+			_size++;
+			_str[_size] = '\0';
+		}
+
+		void append(const char* str)
+		{
+			size_t len = strlen(str);
+			if (_size + len > _capacity)
+			{
+				reserve(_size + len);
+			}
+
+			strcpy(_str + _size, str);
+			_size += len;
+		}
+
+		string& operator+=(char ch)
+		{
+			push_back(ch);
+
+			return *this;
+		}
+
+		string& operator+=(const char* str)
+		{
+			append(str);
+
+			return *this;
+		}
+
+		void insert(size_t pos, char ch)
+		{
+			assert(pos <= _size);
+
+			if (_size == _capacity)
+			{
+				size_t newCapacity = _capacity == 0 ? 4 : _capacity * 2;
+				reserve(newCapacity);
+			}
+
+			size_t end = _size + 1;
+			while (end > pos)
+			{
+				_str[end] = _str[end - 1];
+				--end;
+			}
+
+			_str[pos] = ch;
+			_size++;
+		}
+
+		void insert(size_t pos, const char* str)
+		{
+			assert(pos <= _size);
+			size_t len = strlen(str);
+			if (_size + len > _capacity)
+			{
+				reserve(_size + len);
+			}
+
+			int end = _size;
+			while (end >= (int)pos)
+			{
+				_str[end + len] = _str[end];
+				--end;
+			}
+
+			strncpy(_str + pos, str, len);
+			_size += len;
+		}
+
+		void erase(size_t pos, size_t len = npos)
+		{
+			assert(pos < _size);
+
+			if (len == npos || pos + len >= _size)
+			{
+				_str[pos] = '\0';
+				_size = pos;
+			}
+			else
+			{
+				strcpy(_str + pos, _str + pos + len);
+				_size -= len;
+			}
+		}
+
+		void swap(string& s)
+		{
+			std::swap(_str, s._str);
+			std::swap(_size, s._size);
+			std::swap(_capacity, s._capacity);
+		}
+
+		size_t find(char ch, size_t pos = 0)
+		{
+			for (size_t i = pos; i < _size; i++)
+			{
+				if (_str[i] == ch)
+				{
+					return i;
+				}
+			}
+
+			return npos;
+		}
+
+		size_t find(const char* str, size_t pos = 0)
+		{
+			const char* ptr = strstr(_str + pos, str);
+			if (ptr == nullptr)
+			{
+				return npos;
+			}
+			else
+			{
+				return ptr - _str;
+			}
+		}
+
+		string substr(size_t pos = 0, size_t len = npos)
+		{
+			assert(pos < _size);
+			size_t end = pos + len;
+			if (len == npos || pos + len >= _size)
+			{
+				end = _size;
+			}
+
+			string str;
+			str.reserve(end - pos);
+			for (size_t i = pos; i < end; i++)
+			{
+				str += _str[i];
+			}
+
+			return str;
+		}
+	private:
+		size_t _caoacity;
+		size_t _size;
+		size_t _str;
+
+		const static size_t npos = -1;
+
+
+	};
+}
--- string/text.cpp
+++
@@ -1,9 +0,0 @@
-#define  _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include<string>
-using namespace std;
-#include"string.h"
-int main()
-{
-	return 0;
-}
