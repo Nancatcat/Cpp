d20da8436db99bb5195b4dbd7071e84c4927cac0
Nancatcat <2710514756@qq.com>
2024-01-06 19:16:43 +0800

    前缀和模板 (一维模板, 二维模板)

---
+++ 前缀和1/牛客 (二维和前缀模板).cpp
@@ -0,0 +1,24 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n = 0, m = 0, q = 0;
+    cin >> n >> m >> q;
+    vector<vector<int>> arr(n + 1, vector<int>(m + 1));
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            cin >> arr[i][j];
+    vector<vector<long long>> dp(n + 1, vector<long long>(m + 1));
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + arr[i][j] - dp[i - 1][j - 1];
+    int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+    while (q--)
+    {
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << endl;
+    }
+    return 0;
+}
