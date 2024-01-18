e2c9a2cf38289c6247cb8abc891349ecd6a730e3
Nancatcat <2710514756@qq.com>
2024-01-18 15:46:57 +0800

    list实现

---
+++ list/list/list.h
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+#include<list>
+#include<stdio.h>
+using namespace std;
+
+namespace bit
+{
+	template <class T>
+	struct ListNode
+	{
+		 
+	};
+	
+	class list
+	{
+	private:
+		Node* _head;
+	public:
+	};
+}
